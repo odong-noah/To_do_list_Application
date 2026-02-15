dc68e42374d4c11172095519d7062e4fc8f64042
odong-noah <noahodong6@gmail.com>
2026-02-15 15:58:58 +0300

    Initial commit of my dataconnect.php

---
+++ config/dataconnect.php
@@ -0,0 +1,27 @@
+<?php
+date_default_timezone_set('America/Los_Angeles');
+
+$servername = "localhost";
+$dbname     = "to_do_list";
+$username   = "root";
+$password   = "";
+
+try {
+    $conn = new PDO(
+        "mysql:host=$servername;dbname=$dbname;charset=utf8",
+        $username,
+        $password
+    );
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    error_log($e->getMessage());
+    die("Database connection failed.");
+}
+
+// Helper function
+function clean_string($string_val) {
+    if ($string_val === null) {
+        return "";
+    }
+    return strip_tags(filter_var($string_val, FILTER_SANITIZE_SPECIAL_CHARS));
+}
