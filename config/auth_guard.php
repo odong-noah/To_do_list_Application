a387fae4fd8cdf8ae81572cab8d007c68123a166
odong-noah <noahodong6@gmail.com>
2026-02-16 11:16:32 +0300

    Change in my config files

--- config/auth_guard.php
+++ config/auth_guard.php
@@ -1,18 +1,31 @@
 <?php
-require_once 'dataconnect.php';
 
 if (session_status() === PHP_SESSION_NONE) {
+
     session_start();
+
 }
+
+
 
 // Redirect if not logged in
+
 if (!isset($_SESSION['user_id'])) {
+
     header("Location: To_do_list_login.php");
+
     exit();
+
 }
 
+
+
 //(This kills the back-arrow memory)
+
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+
 header("Cache-Control: post-check=0, pre-check=0", false);
+
 header("Pragma: no-cache");
+
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
